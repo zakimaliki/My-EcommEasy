14d3010468d3388646e8b229a1f623f0ba7c4e58
Zaki <zaki.maliki@ilcs.co.id>
2025-12-09 11:50:15 +0700

    feat: Add .npmrc for legacy peer dependency support and remove sign-up prompt from login page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -181,7 +181,6 @@ export default function LoginPage() {
           {/* Footer */}
           <div className="mt-6 text-center">
             <p className="text-gray-600 text-sm">
-              Don't have an account?{' '}
               <Link href="/signup" className="text-blue-600 hover:text-blue-700 font-semibold">
                 Sign up
               </Link>
